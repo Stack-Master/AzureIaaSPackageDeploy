464aaeade4b60e2860557337d8ef67e9ee25ca66
Stack-Master <48837244+Stack-Master@users.noreply.github.com>
2019-05-29 11:05:52 +0100

    Add files via upload

--- PSver5/DeploymentInitiator-GIT.ps1
+++ PSver5/DeploymentInitiator-GIT.ps1
@@ -7,6 +7,9 @@ If(!(Test-Path $Folder))
 }
 Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/PSver5/IaaS-DeployPackagesPSV5.ps1
 Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/PSver5/config.json
+
+#Packages
 Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/JSON/notepadpp.json -Destination $Folder
+Start-BitsTransfer -Source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/JSON/chrome.json -Destination $Folder
 
 & .\IaaS-DeployPackagesPSV5.ps1
