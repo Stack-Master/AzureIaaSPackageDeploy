fd3762a4cf856307a36d666f7cb73ee7f866ed2a
Stack-Master <48837244+Stack-Master@users.noreply.github.com>
2019-05-29 10:58:11 +0100

    Incl JSON Path

--- PSver5/DeploymentInitiator-GIT.ps1
+++ PSver5/DeploymentInitiator-GIT.ps1
@@ -1,5 +1,12 @@
 Import-Module BitsTransfer
+
+$Folder = "$PSScriptRoot\JSON"
+If(!(Test-Path $Folder))
+{
+    New-Item -ItemType Directory -Force -Path $Folder
+}
 Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/PSver5/IaaS-DeployPackagesPSV5.ps1
 Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/PSver5/config.json
+Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/JSON/notepadpp.json -Destination $Folder
 
 & .\IaaS-DeployPackagesPSV5.ps1
