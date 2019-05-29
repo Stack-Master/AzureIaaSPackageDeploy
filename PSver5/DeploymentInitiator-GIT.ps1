a4664d70a6db7bac33d85508420dd9ecaf0b89f7
Stack-Master <48837244+Stack-Master@users.noreply.github.com>
2019-05-29 10:49:49 +0100

    Corrected for PSV5

--- PSver5/DeploymentInitiator-GIT.ps1
+++ PSver5/DeploymentInitiator-GIT.ps1
@@ -2,4 +2,4 @@ Import-Module BitsTransfer
 Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/PSver5/IaaS-DeployPackagesPSV5.ps1
 Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/PSver5/config.json
 
-& .\IaaS-DeployPackages.ps1
+& .\IaaS-DeployPackagesPSV5.ps1
