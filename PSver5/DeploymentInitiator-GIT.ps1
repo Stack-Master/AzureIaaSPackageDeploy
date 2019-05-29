1cb6e3ff979206feba1f8d5acb26852a8271bfdb
Stack-Master <48837244+Stack-Master@users.noreply.github.com>
2019-05-29 11:42:21 +0100

    Add files via upload

--- PSver5/DeploymentInitiator-GIT.ps1
+++ PSver5/DeploymentInitiator-GIT.ps1
@@ -1,15 +1,13 @@
-Import-Module BitsTransfer
 
-$Folder = "$PSScriptRoot\JSON"
-If(!(Test-Path $Folder))
-{
-    New-Item -ItemType Directory -Force -Path $Folder
-}
-Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/PSver5/IaaS-DeployPackagesPSV5.ps1
-Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/PSver5/config.json
+$currentdir = $PSScriptRoot
+$Output = "$currentdir\AzureIaaSPackageDeploy-master.zip"
 
-#Packages
-Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/JSON/notepadpp.json -Destination $Folder
-Start-BitsTransfer -Source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/JSON/chrome.json -Destination $Folder
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile("http://github.com/Stack-Master/AzureIaaSPackageDeploy/archive/master.zip", $Output)
 
-& .\IaaS-DeployPackagesPSV5.ps1
+Expand-Archive "AzureIaaSPackageDeploy-master.zip" -Force
+
+Set-Location "$currentdir\AzureIaaSPackageDeploy-master\"
+
+& .\IaaS-DeployPackages.ps1
+Write-Host "Ran!"
