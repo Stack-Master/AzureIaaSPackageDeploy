eb3c3a03e57ce39aeb1b4eae3042879cf32f9e76
Stack-Master <48837244+Stack-Master@users.noreply.github.com>
2019-05-29 10:47:56 +0100

    Add files via upload

---
+++ PSver5/DeploymentInitiator-GIT.ps1
@@ -0,0 +1,5 @@
+Import-Module BitsTransfer
+Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/PSver5/IaaS-DeployPackagesPSV5.ps1
+Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/PSver5/config.json
+
+& .\IaaS-DeployPackages.ps1
