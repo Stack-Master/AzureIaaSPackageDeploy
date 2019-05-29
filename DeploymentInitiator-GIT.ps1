117f5cf8d9153c24f99ce33d28336e3eb9ec6914
Stack-Master <48837244+Stack-Master@users.noreply.github.com>
2019-05-29 10:49:36 +0100

    Delete DeploymentInitiator-GIT.ps1

--- DeploymentInitiator-GIT.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module BitsTransfer
-Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/PSver5/IaaS-DeployPackagesPSV5.ps1
-Start-BitsTransfer -source https://raw.githubusercontent.com/Stack-Master/AzureIaaSPackageDeploy/master/PSver5/config.json
-
-& .\IaaS-DeployPackagesPSV5.ps1
